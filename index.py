838e96d8bf3d366a9e241de3955efa4e63bfaf83
doniyor209 <doniyortoshev123@gmail.com>
2025-10-05 11:06:05 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,80 @@
+# print("9 - 8")
+# # print ("Mening yoshim ", 2025 - 2009 , "ga teng")
+# print (f"Mening yoshim {2025 - 2009} ga teng")
+
+# print("Assalomu aleukum /nyoshlar va qarilar")
+
+# print("Meninng ismim ", end=='-->>')
+# print("Alijon")
+
+
+
+# print ("Biz bugun pythonni boshladik va juda siqilyammiz",'yaxshi', sep='==')
+
+
+
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(f"{i} x {j} = {i*j}")
+#     print()
+
+# print("      *  ")
+# print("    *   *  ")
+# print("  *       *  ")
+# print("    *   *  ")
+# print("  *       *  ")
+# print("    *   *  ")
+# print("  *       *  ")
+# print("    *   *  ")
+# print("    *   *  ")
+# print("    *   *  ")
+# print("    *   *  ")
+
+
+# print("       .mop")
+# print("      .disliket")
+# print("     noob.proyy")
+# print("    gfhstm.a/")
+# print("    fdgdggf")
+# print("   vbd.df/errvooosl./;ew")
+# print("  dsfsdfddssdddsffgergeadssdstrh")
+# print(" fgfegfegregfrgreggegergergfgykgbttt.")
+# print(" rrhtytyuythytyhtyjukyukykkykufykyt,,t")
+# print(" wef..dsdkf//d.d/.s/..ee/../sdfwftvjj")
+# print(" fgergfgergfgergthk../fwmuiwjjjd")
+# print("r.dw/set/system.notsuppored.dfff")
+# print("dfnvnmvv../;sdajsvwfd.apleiphone./f")
+# print("nkjdfvmdv.,njdfdfvdfvdfdfvdv.iphonefdw")
+# print("fdgmn..efkjsjefjkiis.dddd/fdfdseff")
+# print("fdgndfg.nggeggf/dfdfdfddfdfd")
+# print(" dsfsdfrth.hert.dsffsd../,r")
+# print("  fggd//fd....fdgeolg")
+# print("       ......")
+
+
+
+
+
+
+
+                                     # HOMEWORK                        
+
+
+
+
+
+
+print("Doniyor Toshev")
+
+print("75+25=",75+25)
+
+
+print("Doniyor","Diyor","Sevara",sep=" ★ ")
+
+print("Salom", end=" ")
+print("Dunyo!")
+
+
+
+print("🚀", "PYTHON", "IS", "FUN", sep="🔥", end="✨\n")
+print("😊", "LET'S", "CODE", sep="💻")
